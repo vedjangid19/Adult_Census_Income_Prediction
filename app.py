8aad12a46f246f490f3c261b13fb1abc6cd691da
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-09-02 08:38:46 +0530

    CI-CD Pipeline.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    return 'Hello World from Python Flask!'
+    return 'Hello World from Python Flask Demo!'
 
 if __name__ == "__main__":
     app.run(debug=True)
