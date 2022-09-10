45dfaa9b5583603f5c7d0b710f20d3acf60649e3
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-09-10 19:22:24 +0530

    Data Validation Done.(Day-7)

--- AC_income_prediction/pipeline/pipeline.py
+++ AC_income_prediction/pipeline/pipeline.py
@@ -8,9 +8,10 @@
 
 from threading import Thread
 from AC_income_prediction.component.data_ingestion import DataIngestion
+from AC_income_prediction.component.data_validation import DataValidation
 
 from AC_income_prediction.config.configuration import Configuration
-from AC_income_prediction.entity.artifact_entity import DataIngestionArtifact
+from AC_income_prediction.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from AC_income_prediction.exception import IncomePredictionException
 import sys,os
 
@@ -30,9 +31,13 @@ class Pipeline(Thread):
         except Exception as e:
             raise IncomePredictionException(e,sys) from e 
         
-    def start_data_validation(self)-> DataIngestionArtifact:
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact)-> DataValidationArtifact:
         try:
-            pass
+            data_validation = DataValidation(
+                data_validation_config=self.config.get_data_validation_config(),
+                data_ingestion_artfact=data_ingestion_artifact
+            )
+            return data_validation.initiate_data_validation()
         except Exception as e:
             raise IncomePredictionException(e,sys) from e 
         
@@ -63,5 +68,8 @@ class Pipeline(Thread):
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact,
+            )
         except Exception as e:
             raise IncomePredictionException(e,sys) from e 
